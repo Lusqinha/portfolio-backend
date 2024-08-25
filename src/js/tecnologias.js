297ba28ad760c678f6d95a96cf1e7d5fd7a92f24
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-08-24 23:39:37 -0300

    update: atualização de informações pessoais

--- src/js/tecnologias.js
+++ src/js/tecnologias.js
@@ -1,6 +1,6 @@
 descricoes = {
     "Python": "Uma linguagem de alto nível com sintaxe simples e direta sendo utilizadas em diversas áreas, uma escolha excelente quando o foco é webscrapping e análise de dados.",
-    "Java": "Uma das linguagens de programação mais utilizadas no mundo, que tenho um apreço especial, à utilizo principalmente no desenvolvimento de aplicações desktop e web.",
+    "Ruby": "Ruby é uma linguagem de programação de alto nível, com uma sintaxe simples e elegante, sendo uma excelente escolha para o desenvolvimento de aplicações web.",
     "Go": "Go é uma linguagem de programação de código aberto que facilita a criação de software simples, confiável e eficiente, sendo uma excelente escolha para o desenvolvimento de aplicações de alta performance.",
     "Linux": "Linux é um sistema operacional de código aberto, essencial quando o assunto são servidores, o que o torna indispensável para um desenvolvedor completo.",
     "Git": "Git é um sistema de controle de versões distribuído, utilizado para o versionamento de código, o que a torna essencial para um processo de desenvolvimento organizado e eficiente.",
